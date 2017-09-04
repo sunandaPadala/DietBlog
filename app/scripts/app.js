7caab68d3e4de9855386a2247823fba5b6e4841d
Sunanda Padala <sunanda93.padala@gmail.com>
2017-09-04 15:12:16 +0530

    Implemented pagination.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,72 +21,73 @@ angular
     'ngTouch',
     'ui.router',
     'angularGrid',
-    '720kb.socialshare'
+    '720kb.socialshare',
+    'angularUtils.directives.dirPagination'
   ]).constant('configSettings', {
-   'baseUrl': 'https://right-my-diet.herokuapp.com/',
-   'someElseSetting': 'settingValue'
-   //other setting will also be there.
-}).config(function($stateProvider, $urlRouterProvider) {
+    'baseUrl': 'https://right-my-diet.herokuapp.com/',
+    'someElseSetting': 'settingValue'
+    //other setting will also be there.
+  }).config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/blog');
     $stateProvider.state('main', {
-      abstract: true,
-      url: "",
-      resolve: { 
-      mainviewData: function() { 
-        return { 
-          menuOpen: false,
-          scrollview: false,
-          headerview: false
-        }; 
-      } 
-    },
-      views: {
-        '': {
-          templateUrl: "views/main.html",
-          controller:"mainviewCtrl"
+        abstract: true,
+        url: "",
+        resolve: {
+          mainviewData: function() {
+            return {
+              menuOpen: false,
+              scrollview: false,
+              headerview: false
+            };
+          }
+        },
+        views: {
+          '': {
+            templateUrl: "views/main.html",
+            controller: "mainviewCtrl"
 
-        },
-        'nav@main': {
-          templateUrl: "views/about.html",
-          // controller:"mainviewCtrl"
-        },
-        'footer@main': {
-          templateUrl: "views/footer.html",
-        // controller:"mainviewCtrl"
+          },
+          'nav@main': {
+            templateUrl: "views/about.html",
+            // controller:"mainviewCtrl"
+          },
+          'footer@main': {
+            templateUrl: "views/footer.html",
+            // controller:"mainviewCtrl"
 
-        },
-        'mobileMenu@main':{
-          templateUrl: "views/mobilemenu.html",
-        // controller:"mainviewCtrl"
+          },
+          'mobileMenu@main': {
+            templateUrl: "views/mobilemenu.html",
+            // controller:"mainviewCtrl"
 
-        },
-      }
-    }).state('main.home', {
-      url: '/home',
-      templateUrl: "views/home.html"
-    })
-    // .state('main.about', {
-    //   url: '/about',
-    //   templateUrl: "views/about.html"
-    // })
-    .state('main.blog', {
-      url: '/blog',
-      views: {
-        '': {
-          templateUrl: "views/blog.html",
-          controller: 'MainCtrl'
-        },
-        'grid@main.blog': {
-          templateUrl: "views/blogGrid.html",
-          controller: "MainCtrl"
-        },
-        'right@main.blog': {
-          templateUrl: "views/blogRight.html"
+          },
         }
-      }
-    }).state('main.blogDetails', {
-      url: '/blogDetails',
-      templateUrl: "views/blogDetails.html",
-      controller: "blogDetailsCtrl"
-    });
+      }).state('main.home', {
+        url: '/home',
+        templateUrl: "views/home.html"
+      })
+      // .state('main.about', {
+      //   url: '/about',
+      //   templateUrl: "views/about.html"
+      // })
+      .state('main.blog', {
+        url: '/blog',
+        views: {
+          '': {
+            templateUrl: "views/blog.html",
+            controller: 'MainCtrl'
+          },
+          'grid@main.blog': {
+            templateUrl: "views/blogGrid.html",
+            controller: "MainCtrl"
+          },
+          'right@main.blog': {
+            templateUrl: "views/blogRight.html"
+          }
+        }
+      }).state('main.blogDetails', {
+        url: '/blogDetails',
+        templateUrl: "views/blogDetails.html",
+        controller: "blogDetailsCtrl"
+      });
   });
