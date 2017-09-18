bcc01086b1c759b9437fa1a593e8b3392412d4da
Sunanda Padala <sunanda93.padala@gmail.com>
2017-09-18 22:04:52 +0530

    tags url change

--- app/scripts/controllers/tagsCtrl.js
+++ app/scripts/controllers/tagsCtrl.js
@@ -1,5 +1,5 @@
 'use strict';
 angular.module('dietBlog').controller('tagsCtrl', ['$scope', 'tagsList', function($scope, tagsList) {
   $scope.tagsList = tagsList.data;
-  console.log($scope.tagsList);
+
 }]);
