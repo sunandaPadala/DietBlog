cdbc465f03b61ec2eecc3db47906d8fb935427c1
sunandaPadala <sunanda93.padala@gmail.com>
2017-09-28 21:47:45 +0530

    Tags Design

--- app/scripts/controllers/tagsCtrl.js
+++ app/scripts/controllers/tagsCtrl.js
@@ -3,6 +3,88 @@ angular.module('dietBlog').controller('tagsCtrl', ['$scope', 'tagsList', 'tagsSe
   $scope.tagsList = tagsList.data;
 
   $scope.alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  /* $scope.alphabet = [{
+     letter: 'a',
+     close: false
+   }, {
+     letter: 'b',
+     close: false
+   }, {
+     letter: 'c',
+     close: false
+   }, {
+     letter: 'd',
+     close: false
+   }, {
+     letter: 'e',
+     close: false
+   }, {
+     letter: 'f',
+     close: false
+   }, {
+     letter: 'g',
+     close: false
+   }, {
+     letter: 'h',
+     close: false
+   }, {
+     letter: 'i',
+     close: false
+   }, {
+     letter: 'j',
+     close: false
+   }, {
+     letter: 'k',
+     close: false
+   }, {
+     letter: 'l',
+     close: false
+   }, {
+     letter: 'm',
+     close: false
+   }, {
+     letter: 'n',
+     close: false
+   }, {
+     letter: 'o',
+     close: false
+   }, {
+     letter: 'p',
+     close: false
+   }, {
+     letter: 'q',
+     close: false
+   }, {
+     letter: 'r',
+     close: false
+   }, {
+     letter: 's',
+     close: false
+   }, {
+     letter: 't',
+     close: false
+   }, {
+     letter: 'u',
+     close: false
+   }, {
+     letter: 'v',
+     close: false
+   }, {
+     letter: 'w',
+     close: false
+   }, {
+     letter: 'x',
+     close: false
+   }, {
+     letter: 'y',
+     close: false
+   }, {
+     letter: 'z',
+     close: false
+   }];*/
+  /*$scope.status = {
+    isOpen: true
+  };*/
   $scope.getTagArticles = function(tagName) {
     $state.go('main.tagArticles', { tagName: tagName });
   };
